b317ce8b53764726dbad311e14143155563de434
ValeriaBelyaeva <iresu1409@yandex.ru>
2023-07-17 18:09:41 +0300

    made game base

---
+++ raycasting.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import map
+from settings import  *
+
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+
+    def ray_cast(self):
+        pass
+
+    def update(self):
+        self.rat_cast()
