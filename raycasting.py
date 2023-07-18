2c5a94ce738f39e86733ac794f331326316505ec
ValeriaBelyaeva <iresu1409@yandex.ru>
2023-07-18 23:04:20 +0300

    start add 3d

--- raycasting.py
+++ raycasting.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import map
-from settings import  *
+from settings import *
 
 
 class RayCasting:
@@ -8,7 +8,24 @@ class RayCasting:
         self.game = game
 
     def ray_cast(self):
-        pass
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map
+
+        ray_angle = self.game.argle - HALF_FOV + 0.0001
+        for ray in range(NOM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            # verticals
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+            depth_vert = (x_vert - ox) / cos_a
+            y_vert = oy + depth_vert * sin_a
+
+            delta_depth = dx / cos_a
+            dy = delta_depth * sin_a
+
+            ray_angle += DELTA_ANGLE
 
     def update(self):
         self.rat_cast()
