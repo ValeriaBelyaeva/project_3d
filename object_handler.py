170a8a65945bba9ba06a1bd8dff25ea0958a4d5e
ValeriaBelyaeva <iresu1409@yandex.ru>
2023-08-20 22:30:57 +0300

    spawn npc

--- object_handler.py
+++ object_handler.py
@@ -1,19 +1,30 @@
 from sprite_object import *
+from npc import *
 
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
-        self.static_sprite_path = 'textures/static_sprite/'
-        self.anim_sprite_path = 'textures/animated_sprite/'
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
+        self.static_sprite_path = 'resources/textures/static_sprite/'
+        self.anim_sprite_path = 'resources/textures/animated_sprite/'
         add_sprite = self.add_sprite
+        add_npc = self.add_npc
 
         # sprite map
         add_sprite(SpriteObject(game))
         add_sprite(AnimatedSprite(game))
 
+        # npc map
+        add_npc(NPC(game))
+
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
